9b5fab9173bb2eaf6e343e6da9e310043c161221
panarno <78936893+panarno@users.noreply.github.com>
2023-01-26 23:30:43 -0800

    added all logic and requirements

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Ticket Eligibility - A JavaScript Web Form Application
+Checks if the submittor is old enough or has enough to buy the tickets they want.
+
+## Changes from Original Assignment
+Instead of using a prompt function, a form was used instead. Also the goal was to reduce amount of code needed.
--- script.js
+++ script.js
@@ -31,16 +31,69 @@ submitInput.value = "Check Eligibility"
 const summaryInput = document.getElementById("input-summary")
 displayInformation()
 
+// initialize result
+
+const resultDisplay = document.getElementById("result")
+
+// function that displays user information
 function displayInformation() {
 
     summaryInput.innerHTML = ""
-    summaryInput.innerHTML = "<h4>User Information</h4>"
-    summaryInput.innerHTML += "<ul>"
-    summaryInput.innerHTML += "<li>Name: " + firstNameInput.value + ` ` + lastNameInput.value + "</li>"
-    summaryInput.innerHTML += "<li>Age: " + ageInput.value + "</li>"
-    summaryInput.innerHTML += "<li>Quantity of Tickets: " + quantityInput.value + "</li>"
-    summaryInput.innerHTML += "<li>Users' Amount of Cash: " + cashInput.value + "</li>"
-    summaryInput.innerHTML += "</ul>"
+    summaryInput.innerHTML += "<h4>User Information</h4>"
+    summaryInput.innerHTML += "<p>Name: " + firstNameInput.value + ` ` + lastNameInput.value + "</p>"
+    summaryInput.innerHTML += "<p>Age: " + ageInput.value + "</p>"
+    summaryInput.innerHTML += "<p>Quantity of Tickets: " + quantityInput.value + "</p>"
+    summaryInput.innerHTML += "<p>Users' Amount of Cash: " + cashInput.value + "</p>"
+}
+
+// function that calculates eligibility 
+function calculateEligibility(){
+    console.log("Calculating...")
+
+    let html = "<h4>Results</h4>"
+    html += "<p>"
+
+    // check variables
+    if(!firstNameInput.value){
+        html += "First Name is Empty. "
+    }else{
+        html += firstNameInput.value + " "
+    }
+    if(!lastNameInput.value){
+        html += "Last Name is Empty. "
+    }else{
+        html += lastNameInput.value + " "
+    }
+
+    if(firstNameInput.value && lastNameInput.value){
+        html += "is looking to get some tickets."
+    }
+
+    html += "</p>"
+
+    // check numbers
+    if(ageInput.value < 16){
+        html += "<p>" + ageInput.value + " is too low. You need to be at least 16.</p>"
+    }else{
+        html += "<p>" + ageInput.value + " is old enough.</p>"
+    }
+
+    let ticketCost = 14
+
+    if((quantityInput.value * ticketCost) > cashInput.value){
+        html += "<p>" + quantityInput.value + " is not enough to purchase" + quantityInput.value + " tickets. </p>"
+    }else{
+        html += "<p>" + cashInput.value + " is enough for " + quantityInput.value +  " tickets. </p>"
+    }
+
+    if(ageInput.value > 16 && (quantityInput.value * ticketCost) > cashInput.value){
+        html += "<p><b>Congratulations. Here are your tickets.</b></p>"
+    }else{
+        html += "<p><b>Sorry, you didn't make at least one requirement.</b></p>"
+    }
+
+    resultDisplay.innerHTML = html
+
 }
 
 // event triggers
@@ -48,6 +101,7 @@ function displayInformation() {
 submitInput.addEventListener("click", function () {
     console.log("Submit Button Clicked.")
     displayInformation()
+    calculateEligibility()
 })
 
 
