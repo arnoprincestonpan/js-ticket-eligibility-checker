71c6ebeb572d91806b7d4cf3b9363e3716497423
arnoprincestonpan <arnopan@Arnos-MBP.lan>
2023-01-26 20:16:36 -0800

    completed form submission, not yet manipulated the data

--- script.js
+++ script.js
@@ -29,14 +29,25 @@ submitInput.value = "Check Eligibility"
 // initialize input summary
 
 const summaryInput = document.getElementById("input-summary")
-summaryInput.innerHTML = "Summary of Form here."
+displayInformation()
 
+function displayInformation() {
 
+    summaryInput.innerHTML = ""
+    summaryInput.innerHTML = "<h4>User Information</h4>"
+    summaryInput.innerHTML += "<ul>"
+    summaryInput.innerHTML += "<li>Name: " + firstNameInput.value + ` ` + lastNameInput.value + "</li>"
+    summaryInput.innerHTML += "<li>Age: " + ageInput.value + "</li>"
+    summaryInput.innerHTML += "<li>Quantity of Tickets: " + quantityInput.value + "</li>"
+    summaryInput.innerHTML += "<li>Users' Amount of Cash: " + cashInput.value + "</li>"
+    summaryInput.innerHTML += "</ul>"
+}
 
 // event triggers
 
-submitInput.addEventListener("click", function(){
-
+submitInput.addEventListener("click", function () {
+    console.log("Submit Button Clicked.")
+    displayInformation()
 })
 
 
